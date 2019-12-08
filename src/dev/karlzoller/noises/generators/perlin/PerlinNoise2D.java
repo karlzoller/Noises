3744f0a805df3c42055a4a0852b08a363bd10974
Karl Zoller <karlzoller@users.noreply.github.com>
2019-12-08 00:56:25 -0800

    Simplified numpty checkerboard math

--- src/dev/karlzoller/noises/generators/perlin/PerlinNoise2D.java
+++ src/dev/karlzoller/noises/generators/perlin/PerlinNoise2D.java
@@ -7,6 +7,6 @@ public class PerlinNoise2D implements NoiseGenerator2D {
     @Override
     public double getValue(int x, int y) {
         //TODO: Implement proper Perlin Noise, for now Checkerboard!
-        return (x / 30) % 2 == 0 && (y / 30) % 2 != 0 || (x / 30) % 2 != 0 && (y / 30) % 2 == 0 ? 255 : 0;
+        return (x / 30) % 2 != (y/ 30) % 2 ? 255 : 0;
     }
 }
